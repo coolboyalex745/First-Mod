56ab5d72171034255f053a73cd4376fed64a1688
Kirito5391 <96701913+coolboyalex745@users.noreply.github.com>
2025-05-15 23:43:03 +0200

    started on item

---
+++ src/main/java/net/kirito5391/firstmod/item/ModItems.java
@@ -0,0 +1,14 @@
+package net.kirito5391.firstmod.item;
+
+import net.kirito5391.firstmod.FirstMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MOD_ID);
+
+
+    
+}
