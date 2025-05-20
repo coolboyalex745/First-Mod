4516294d14b901c5b382a5df9349607cff7c481e
Kirito5391 <96701913+coolboyalex745@users.noreply.github.com>
2025-05-20 20:49:52 +0200

    started on magic block

--- src/main/java/net/kirito5391/firstmod/block/custom/MagicBlock.java
+++ src/main/java/net/kirito5391/firstmod/block/custom/MagicBlock.java
@@ -1,6 +1,20 @@
 package net.kirito5391.firstmod.block.custom;
 
+import net.kirito5391.firstmod.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.client.event.sound.SoundEvent;
 
 public class MagicBlock extends Block {
 
@@ -8,5 +22,22 @@ public class MagicBlock extends Block {
         super(properties);
     }
 
-    
+    @Override
+    protected InteractionResult useWithoutItem(BlockState pState, Level pLevel, BlockPos pPos,
+                                               Player pPlayer, BlockHitResult pHitResult) {
+
+        pLevel.playSound(pPlayer, pPos, SoundEvents.AMETHYST_CLUSTER_PLACE, SoundSource.BLOCKS, 1f, 1f);
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
+        if(pEntity instanceof ItemEntity itemEntity) {
+            if(itemEntity.getItem().getItem() == ModItems.RAW_ALEXANDRITE.get()) {
+                itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
+            }
+        }
+
+        super.stepOn(pLevel, pPos, pState, pEntity);
+    }
 }
